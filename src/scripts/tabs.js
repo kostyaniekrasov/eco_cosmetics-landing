eb25d5ae919c21a72acb3ff46a5526e047c2f264
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-06-13 17:09:57 +0300

    updated code

--- src/scripts/tabs.js
+++ src/scripts/tabs.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const tabs = document.querySelectorAll(".shop__tab-box--button");
-const allContent = document.querySelectorAll(".content");
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".shop__tab-box--button");
+  const allContent = document.querySelectorAll(".content");
 
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", (e) => {
-    tab.classList.remove("active");
-    tab.classList.add("active");
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", (e) => {
+      tabs.forEach((t) => t.classList.remove("active"));
+      tab.classList.add("active");
 
-    const line = document.querySelector(".shop__tab-box--line");
+      const line = document.querySelector(".shop__tab-box--line");
+      line.style.width = e.target.offsetWidth + "px";
+      line.style.left = e.target.offsetLeft + "px";
 
-    line.style.width = e.target.offsetWidth + "px";
-    line.style.left = e.target.offsetLeft + "px";
-
-    Array.from(allContent).forEach((content) => {
-      content.classList.remove("content__active");
+      allContent.forEach((content) =>
+        content.classList.remove("content__active")
+      );
+      allContent[index].classList.add("content__active");
     });
-
-    allContent[index].classList.add("content__active");
   });
 });
